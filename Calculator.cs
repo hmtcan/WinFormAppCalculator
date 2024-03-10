1ebaa4f5074c35fc51e9ee3ff85f90c7a31856d1
Hamit Can SAYILGAN <hamitcansayilgan@gmail.com>
2024-03-10 22:34:31 +0300

    - Comment Line: Added code comments; achievement and scope.

--- Calculator.cs
+++ Calculator.cs
@@ -8,6 +8,11 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
+// Amaç: 4 işlem hesap makinesi
+// Kazanım & Kapsam: Bool operation ile rakam girişi veya sayıların işleme tabi tutulup, tutulmadığını mantıksal olarak kavrama
+//   Tek bir buton click event'i ile rakam yazdırma, işlem operatörlerini işlevsel hale getirme
+//   Tip dönüştürme ve Convert etme (String-Int)
+
 
 namespace WinFormAppCalculator
 {
@@ -18,10 +23,12 @@ namespace WinFormAppCalculator
 			InitializeComponent();
 		}
 
-		double value = 0; 
-		string operation = ""; 
-		bool operation_pressed = false; 
+		double value = 0; // Sonuçları tutsun
+		string operation = ""; // +, -, *, / İşaretini tutsun
+		bool operation_pressed = false; // False = işlem operatör butonlarına basılmamış, yani rakam girişi yapılmaya devam ediyor  
+										//bool operation_pressed = true; // true = rakam butonuna basılmamış, işlem operatörleriyle işlem yapılıyor. 
 
+		//Rakam butonlarına basıldığında;
 		private void button0_Click(object sender, EventArgs e)
 		{
 
@@ -30,20 +37,22 @@ namespace WinFormAppCalculator
 				txt_Result.Clear();
 			}
 
-			
+			// Cast(ing) = tip dönüştürme
+			// ConvertToInt32(txt_sonuc) 
 
-			Button button = sender as Button; 
+			Button button = sender as Button; // ConvertToButton(sender) // 1. yol
+											  //Button button = (Button)sender; // 2. yol
 
 			txt_Result.Text = txt_Result.Text + button.Text;
-			operation_pressed = false; 
+			operation_pressed = false; // Rakam butonuna basıldığı için
 
 		}
-		
+		// İşlem operatör butonlarına basıldığında;
 		private void button3_Click(object sender, EventArgs e)
 		{
 			Button button = sender as Button;
-			operation = button.Text; 
-			operation_pressed = true;
+			operation = button.Text; // İşlem operatör işaretlerini bu değişkene atadım
+			operation_pressed = true; // İşlem operatör butonlarına basılıyor
 			value = double.Parse(txt_Result.Text);
 
 		}
@@ -81,10 +90,9 @@ namespace WinFormAppCalculator
 
 				default:
 					break;
-
 			}
 		}
 
 
-		}
+	}
 }
